44ab92a0c8f267bdb6ad3026842f5cbda331013b
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-24 08:21:45 +0300

    api bağlantısı ve axios kodları

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
-import axios from 'axios';
-
-const options = {
-  method: 'GET',
-  url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
-  headers: {
-    'X-RapidAPI-Key': '9a37cc7818mshed6ac7313d7f93cp1fb439jsnc58358def347',
-    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-  }
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url = "https://api.openweathermap.org/data/2.5/";
+    const key = "9f3441674da2adb06b13700d6ff19b5f";
+    let city = "bursa";
+    let query = `${url}weather?q=${city}&appid=${key}&units=metric&lang=tr`;
+    const fetchData = async () => {
+      const response = await axios.get(query);
+      setData(response);
+    };
+    fetchData();
+  }, []);
+  console.log(data);
   return <div className="App"></div>;
 }
 
