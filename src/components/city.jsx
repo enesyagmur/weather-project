8323bed2ad1a36a5f62d39a8573dacb6991d6113
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-24 09:29:03 +0300

    city componentinin kaba halini yaptık

--- src/components/city.jsx
+++ src/components/city.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./style/city.css";
+import { BsFillCloudRainFill } from "react-icons/bs";
+import { TiWeatherPartlySunny } from "react-icons/ti";
 
 const City = () => {
   return (
@@ -7,13 +9,20 @@ const City = () => {
       <div className="searchCity">
         <input type="text" />
       </div>
-      <div className="cityStatusIcon">İcon</div>
-      <p className="cityDegree">derece</p>
-      <div>
-        <p>Gün</p> <p>Saat</p>
+      <div className="cityStatusIcon">
+        <TiWeatherPartlySunny />
       </div>
-      <div>icon + Bulutlu</div>
-      <div>icon yağmur + yağmur ihtimali</div>
+      <p className="cityDegree">12'c</p>
+      <div className="time">
+        <p>Monday</p> <p>16:00</p>
+      </div>
+      <div className="cityStatus">
+        {" "}
+        <TiWeatherPartlySunny /> Bulutlu
+      </div>
+      <div>
+        <BsFillCloudRainFill /> %30
+      </div>
     </div>
   );
 };
